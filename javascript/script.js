c37d4cd71e64f33506dd126ff6d5341a05c2aebe
Jakub Zuber <jakub.zuber19@gmail.com>
2022-09-14 23:29:00 +0200

    .

--- javascript/script.js
+++ javascript/script.js
@@ -23,6 +23,37 @@
         document.querySelector(".js-tasks").innerHTML = htmlAndClssInner;
     };
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push(
+            {
+                content: newTaskContent,
+            }
+        )
+        addTaskToList();
+    }
 
-    addTaskToList()
+    const onFormSubmit = (event) => {
+            event.preventDefault();
+
+            const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+            if (newTaskContent === "") {
+                returnl
+            };
+            
+            addNewTask(newTaskContent);
+        };
+    
+
+    const init = () => {
+        addTaskToList();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
+
+        
+    };
+    init();
+    
 };
